d76286cfbd7bde028a9d6f7ac2dc3d0c04195e25
venkatesh <vs498@njit.edu>
2018-03-30 16:23:59 -0400

    pages controller missed now added

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,15 +9,7 @@ class PagesController extends Controller
     function contact() {
         return view('pages.contact');
     }
-    function store(Request $request) {
-        $name = $request->name;
 
-        return redirect()->route('thanks',['name' => $name ]);
-    }
-    function thanks($name, Request $request) {
-        echo 'ThankYou';
-        //return view('pages.thankyou')->with(compact('name'));
-    }
     function about() {
         return view('pages.about');
     }
